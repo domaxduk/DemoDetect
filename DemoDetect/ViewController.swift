17d08679e31eceffb570f0417fd67e44af2a76a2
domaxduk <domaxduk@gmail.com>
2024-12-24 09:44:24 +0700

    Initial Commit

---
+++ DemoDetect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoDetect
+//
+//  Created by Duc apple  on 24/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
